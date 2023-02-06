1bcf462820fdd568445375227075dd12037ae337
Vladimirs <98471773+Golotniks94@users.noreply.github.com>
2023-02-06 16:12:09 +0200

    Push!!!

---
+++ src/components/ContactsList/ContactsList.styled.jsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  padding: 20px 40px;
+  background-color: #e4f9f4;
+`;
+
+export const Title = styled.h2`
+  margin-bottom: 20px;
+  text-align: center;
+`;
+
+export const Empty = styled.p`
+  text-align: center;
+`;
